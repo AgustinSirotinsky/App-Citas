dbff6c7bdada3b73df8425e9b0e5b5cc2337daeb
AGUSTÍN SIROTINSKY <46645963@ort>
2023-05-10 09:05:16 -0300

    10/5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title/>
+      <div class="container">
+        <div class="row">
+        <div class="one-half column">
+        <CrearCita/>
+        </div>
+        </div>
+      </div>
     </div>
   );
+}
+function Title() {
+  return (
+    <h1>hola soy el h1</h1>
+  )
+}
+function CrearCita(){
+  return (
+  <div className="CrearCita">
+    <h2>Crear mi Cita</h2>
+        <form>
+          <label>Nombre de la mascota</label>
+          <input type="text" name="mascota" class="u-full-width" placeholder="Nombre Mascota" value=""></input>
+          <label>Nombre Dueño</label><input type="text" name="propietario" class="u-full-width" placeholder="Nombre dueño de la mascota" value=""></input>
+          <label>Fecha</label><input type="date" name="fecha" class="u-full-width" value=""></input>
+          <label>hora</label><input type="time" name="hora" class="u-full-width" value=""></input>
+          <label>Sintomas</label><textarea name="sintomas" class="u-full-width"></textarea>
+          <button type="submit" class="u-full-width button-primary">Agregar Cita</button>
+        </form> 
+  </div>
+  )
 }
 
 export default App;
