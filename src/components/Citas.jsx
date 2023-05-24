b38f39e823fc2f025e081d9eebb997ca589ed7a7
AGUSTÍN SIROTINSKY <46645963@ort>
2023-05-24 08:15:25 -0300

    commit rapido arregle algunas cosas ademas de agregar el fragment y jsx,es simplemente un avance rapido

---
+++ src/components/Citas.jsx
@@ -0,0 +1,18 @@
+import React,{Fragment} from "react";
+
+function Ingresos(paciente){
+    return (
+      <Fragment>
+        <div class="cita">
+            <p>Mascota: <span>{paciente.nombre}</span></p>
+            <p>Dueño: <span>{paciente.dueno}</span></p>
+            <p>Fecha: <span>{paciente.fecha}</span></p>
+            <p>Hora: <span>{paciente.hora}</span></p>
+            <p>Sintomas: <span>{paciente.sintomas}</span></p>
+            <button class="button elimnar u-full-width">Eliminar ×</button>
+        </div>
+      </Fragment>
+    );
+  }
+  
+  export default Ingresos;
